46b40bb51d29b4dcf3e47a1997b46128ff854b65
adyhariri <mahmoudalhariri1994@gmail.com>
2021-06-27 13:25:54 +0300

    review function

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 'use strict'
+
 const express = require('express')
 const cors=require('cors');
 require('dotenv').config();
 const app = express();
 app.use(cors());
+app.use(express.json());
+const reviewFunction=require('./review')
 
 
 app.get('/', function (req, res) {
     res.send('Hello MoveNest')
   });
 
-  app.listen(process.env.PORT)
+  
+app.post('/review',reviewFunction.addReviews);
+app.delete('/review',reviewFunction.deleteReview);
+app.put('/review',reviewFunction.updateReview);
+
+
+app.listen(process.env.PORT)
